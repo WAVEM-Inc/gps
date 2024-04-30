06b5201e4d328931f8d208376c63d835554865cd
nuc-bt <felicita64@gmail.com>
2024-04-30 16:56:13 +0900

    feat : add ublox_filter 작업중

---
+++ ublox_filter/ublox_filter/include/ublox_filter/ublox_filter.hpp
@@ -0,0 +1,29 @@
+#ifndef UBLOX_FILTER_LISTENER_HPP__
+#define UBLOX_FILTER_LISTENER_HPP__
+#include <iostream>
+#include <fstream>
+#include <fcntl.h>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/nav_sat_fix.hpp"
+#include "sensor_msgs/msg/nav_pvt.hpp"
+#include <unistd.h>
+using GpsMSG = sensor_msgs::msg::NavSatFix ;
+using PvtMSG = ublox_msgs::msg::NavPVT ;
+class GpsLogger : public rclcpp::Node{
+	int fd_ori_log;
+	int fd_ori_gpx;
+	int fd_filter_log;
+	int fd_filter_gpx;
+	time_t now;
+	struct tm *fix_t;
+    private :
+        rclcpp::Subscription<GpsMSG>::SharedPtr sub_ori_gps_;
+        rclcpp::Subscription<GpsMSG>::SharedPtr sub_filter_gps_;
+        void gps_callback(const std::shared_ptr<GpsMSG> fix);
+        void gps_filter_callback(const std::shared_ptr<GpsMSG> fix);
+    public :
+        GpsLogger();
+	
+
+};
+#endif
